10bf85ac284054ea99dbcbb4db3d7621e9b30b71
RK <gorkvilar@gmail.com>
2023-09-24 15:39:04 +0200

    added custom theme

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'About': "# **GRKdev** v1"
+        'About': "# **GRKdev** v1.1"
     }
 )
 
